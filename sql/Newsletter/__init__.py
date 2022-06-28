9a481fc764ad937f573c23e8d8455e5e3b5f769f
Emilie <emiliemontpre@outlook.com>
2022-06-28 08:22:10 +0200

    Newslettre

---
+++ sql/Newsletter/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@Localhost/mvc'
+app.config['SECRET_KEY'] = "random string"
+
+app.config['MAIL_SERVER'] = 'smtp-yuniq.alwaysdata.net'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'yuniq@alwaysdata.net'
+app.config['MAIL_PASSWORD'] = 'Admkyuniq'
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USE_TLS'] = True
+
+app.config['BCRYPT_LOG_ROUNDS'] = 6
+app.config['BCRYPT_HASH_IDENT'] = '2b'
+app.config['BCRYPT_HANDLE_LONG_PASSWORDS'] = False
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+from Newsletter import route
+
+
